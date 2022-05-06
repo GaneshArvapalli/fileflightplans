bcbb7c8e72d63e70430c5780469e91dc8be6dfc5
Ganesh Arvapalli <ganesh.arvapalli@gmail.com>
2022-05-05 23:53:14 -0400

    Initial commit from moving code over

---
+++ kml_shrink_test.py
@@ -0,0 +1,105 @@
+# Ganesh Arvapalli
+# 5/4/2022
+
+import numpy as np
+from shapely.geometry import Point, LineString, Polygon
+from fastkml import kml, styles
+from pyproj import Proj, Transformer
+
+
+# Read in KML file
+kml_file = input("Which file do you want to use? ")
+
+# The +7.7 comes from accounting for wind
+max_speed = float(input("What speed will your vehicle be going in m/s? "))# + 7.716667 
+
+with open(kml_file, 'rt', encoding="utf-8") as myfile:
+    doc=myfile.read()
+    k = kml.KML()
+    k.from_string(bytes(doc, encoding='utf8'))
+
+# Get the boundary geometry
+features = list(k.features())
+f2 = list(features[0].features())
+red_bounds = f2[0].geometry
+
+print("RED")
+# Reproject to meter space
+transformer = Transformer.from_crs("epsg:4326", "epsg:3857")
+meter_points = []
+x, y = red_bounds.exterior.coords.xy
+for i,j in zip(x,y):
+    print(i, j)
+    xp,yp = transformer.transform(i,j)
+    meter_points.append((xp, yp))
+
+meter_polygon = Polygon([[p[0], p[1]] for p in meter_points])
+
+# Shrink inward proportional to max speed
+yellow = meter_polygon.buffer(max_speed * -5, join_style=2)
+green = meter_polygon.buffer(max_speed * -7, join_style=2)
+
+# Testing parallel offset method
+# yellow_line = meter_polygon.exterior.parallel_offset(distance=-40, side='left')
+# yellow = Polygon(yellow_line)
+# green_line = meter_polygon.exterior.parallel_offset(distance=-80, side='left')
+# green = Polygon(green_line)
+
+print("YELLOW")
+transformer = Transformer.from_crs("epsg:3857", "epsg:4326")
+x, y = yellow.exterior.coords.xy
+yellow_ge_points = []
+for i,j in zip(x,y):
+    xp,yp = transformer.transform(i,j)
+    yellow_ge_points.append((xp, yp))
+    print(xp, yp)
+
+yellow_ge_polygon = Polygon([[p[0], p[1]] for p in yellow_ge_points])
+
+print("GREEN")
+# inProj = Proj('epsg:3857')
+# outProj = Proj('epsg:4326')
+transformer = Transformer.from_crs("epsg:3857", "epsg:4326")
+x, y = green.exterior.coords.xy
+green_ge_points = []
+for i,j in zip(x,y):
+    xp,yp = transformer.transform(i,j)
+    green_ge_points.append((xp, yp))
+    print(xp, yp)
+
+green_ge_polygon = Polygon([[p[0], p[1]] for p in green_ge_points])
+
+# Write out to file
+outfile = kml_file[:-4] + "_auto.kml"
+
+k = kml.KML()
+ns = '{http://www.opengis.net/kml/2.2}'
+d = kml.Document(ns, 'RRA_Test_2.kml')
+k.append(d)
+f = kml.Folder(ns=ns, name='Bounds')
+d.append(f)
+
+ls = styles.LineStyle(ns, color='ff0000ff', width=4)
+ps = styles.PolyStyle(ns, fill=0)
+s = styles.Style(styles=[ls,ps])
+rp = kml.Placemark(ns=ns, name='Red Bounds', styles=[s])
+rp.geometry = red_bounds
+
+ls = styles.LineStyle(ns, color='ff00ffff', width=4)
+ps = styles.PolyStyle(ns, fill=0)
+s = styles.Style(styles=[ls,ps])
+yp = kml.Placemark(ns=ns, name='Yellow Bounds', styles=[s])
+yp.geometry = yellow_ge_polygon
+
+ls = styles.LineStyle(ns, color='ff00ff00', width=4)
+ps = styles.PolyStyle(ns, fill=0)
+s = styles.Style(styles=[ls,ps])
+gp = kml.Placemark(ns=ns, name='Green Bounds', styles=[s])
+gp.geometry = green_ge_polygon
+
+f.append(rp)
+f.append(yp)
+f.append(gp)
+
+with open(outfile, 'w', encoding="utf-8") as myfile:
+    myfile.write(k.to_string(prettyprint=True))
